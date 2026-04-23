1de6927dbd652ff09f8c29c1875ac777266bf939
abhishek764 <abhishek.sphs01@gmail.com>
2026-04-23 23:48:16 +0530

    adding music models

--- backend/main.py
+++ backend/main.py
@@ -32,4 +32,53 @@ class MusicGenServer():
     @modal.enter()
     def load_model(self):
         from acestep.pipelines import ACEStepPipeline
+        from transformers import AutoTokenizer, AutoModelForCausalLM
+        from diffusers import AutoPipelineForText2Audio
+        import torch
 
+        #music generation model from ACE-Step
+        self.music_model = ACEStepPipeline(
+            checkpoint_dir = "/models",
+            dtype = "bfloat16",
+            torch_compile = False,
+            cpu_offload = False,
+            overlapped_decode = False
+        )
+
+        #large language model for text processing
+
+        self.tokenizer = AutoTokenizer.from_pretrained(model_id)
+
+        self.llm_model = AutoModelForCausalLM.from_pretrained(
+            model_id,
+            torch_dtype="auto",
+            device_map="auto",
+            cache_dir="/.cache/huggingface"
+        )
+
+ # Stable Diffusion Model (thumbnails)
+        self.image_pipe = AutoPipelineForText2Image.from_pretrained(
+            "stabilityai/sdxl-turbo", torch_dtype=torch.float16, variant="fp16", cache_dir="/.cache/huggingface")
+        self.image_pipe.to("cuda")
+
+
+
+@app.local_entrypoint()
+def main():
+    server = MusicGenServer()
+    endpoint_url = server.generate_with_described_lyrics.get_web_url()
+
+    request_data = GenerateWithDescribedLyricsRequest(
+        prompt="rave, funk, 140BPM, disco",
+        described_lyrics="lyrics about water bottles",
+        guidance_scale=15
+    )
+
+    payload = request_data.model_dump()
+
+    response = requests.post(endpoint_url, json=payload)
+    response.raise_for_status()
+    result = GenerateMusicResponseS3(**response.json())
+
+    print(
+        f"Success: {result.s3_key} {result.cover_image_s3_key} {result.categories}")
